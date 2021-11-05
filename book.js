c2fd9ca4b9e913b6b1841261d32ae1b79fca5ffd
SHihozs <88846962+SHihozs@users.noreply.github.com>
2021-11-05 17:28:42 +0700

    Update book.js

--- book.js
+++ book.js
@@ -39,3 +39,42 @@ book1.restock(12);
 console.log(book1.getAvailability());
 book1.sell(17);
 console.log(book1.getAvailability());
+
+
+
+/*function book(Title,Author,ISBN,numCopies){
+    this.Title     = Title;
+    this.Author    = Author;
+    this.ISBN      = ISBN;
+    this.numCopies = numCopies;
+
+    //method
+    this.getAvailability = function(){
+    if(this.numCopies === 0) {
+        return "out of stock";
+    }
+    else if(this.numCopies < 10) {
+        return "low stock";
+    }
+    else {
+    return "in stock";
+    } 
+}
+
+    this.sell = function(numSold=1){
+        this.numCopies -= numSold;
+}
+
+    this.restock = function(numCopiesStocked = 5) {
+        this.numCopies += numCopiesStocked;
+  }
+}
+
+
+let book1 = new book("Doreamon","Nobi",123456,5);
+console.log(book1.getAvailability());
+book1.restock(12);
+console.log(book1.getAvailability());
+book1.sell(17);
+console.log(book1.getAvailability());
+*/
